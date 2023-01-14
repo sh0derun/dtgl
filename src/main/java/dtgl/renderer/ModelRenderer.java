61cc29c60c0c2d34ce958273b27f81422fcf48af
Sh@drun <5242001+sh0derun@users.noreply.github.com>
2023-01-14 22:57:15 +0100

    testing lookAt

--- src/main/java/dtgl/renderer/ModelRenderer.java
+++ src/main/java/dtgl/renderer/ModelRenderer.java
@@ -11,6 +11,7 @@ import dtgl.shader.DefaultUniformsHandler;
 import dtgl.shader.Shader;
 import dtgl.shader.Uniform;
 import dtgl.shader.UniformType;
+import org.joml.Vector3f;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL30;
@@ -51,21 +52,23 @@ public class ModelRenderer extends AbstractRenderer{
 
 		double time = GLFW.glfwGetTime();
 		float radius = 30.0f;
-		float lightPosX = (float) (Math.sin(time) * radius) * (float) (Math.cos(time) * radius);
-		float lightPosY = (float) (Math.sin(time) * radius) * (float) (Math.sin(time) * radius);
-		float lightPosZ = (float) (Math.cos(time) * radius);
+		float lightPosX = 0;
+		float lightPosY = (float) (Math.sin(time*0.5) * radius*0.6);
+		float lightPosZ = -10;
 
-		Mat4 projection = Mat4.perspective(70, 1.0f * window.getWidth() / window.getHeight(), 0.1f, 30);
+		Mat4 projection = Mat4.perspective(40, 1.0f * window.getWidth() / window.getHeight(), 0.1f, 50);
+
+		camera.setPos(new Vec3(lightPosX, lightPosY, lightPosZ));
 
 		List<Uniform> requiredUniforms = new ArrayList<>(Arrays.asList(
 				new Uniform(UniformType.MAT4, "model", Mat4.getTransformationMat(model.getPos(), model.getRot(), model.getAngle(), model.getScale())),
-				new Uniform(UniformType.MAT4, "view", Mat4.translate(Vec3.TOWARD.mult(-10))),
+				new Uniform(UniformType.MAT4, "view", Mat4.lookAt(new Vec3((float) (Math.cos(time*0.5) * radius),(float) (Math.cos(time*0.5) * radius),(float) (Math.sin(time*0.5) * radius)), new Vec3(0,0,0), new Vec3(0,1,0))/*Mat4.translate(Vec3.TOWARD.mult(-10))*/),
 				new Uniform(UniformType.MAT4, "projection", projection)));
 
-		List<Uniform> uniformTextures = Arrays.stream(model.getTextures().orElse(new Texture[]{}))
+		/*List<Uniform> uniformTextures = Arrays.stream(model.getTextures().orElse(new Texture[]{}))
 				.map(texture -> new Uniform(UniformType.SAMPLER_2D, texture.getUniformName(), texture))
 				.collect(Collectors.toList());
-		uniforms.addAll(uniformTextures);
+		uniforms.addAll(uniformTextures);*/
 
 		if (model.getShader().inError) {
 			requiredUniforms.add(new Uniform(UniformType.VEC4, "error_color", Shader.ERROR_COLOR));
@@ -74,7 +77,7 @@ public class ModelRenderer extends AbstractRenderer{
 			Uniform light = new Uniform(UniformType.POINT_LIGHT,
 										"point_light",
 										new PointLight(new Vec3(1, 1, 1),
-										new Vec3(0,0,7/*(float) (Math.cos(time) * radius), 0, (float) (Math.sin(time) * radius) + 10*/)));
+										new Vec3((float) (Math.cos(time) * radius), 0, (float) (Math.sin(time) * radius))));
 
 			requiredUniforms.add(light);
 
