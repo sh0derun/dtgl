70faa4aacafe96855d5e73e7734f94dbf16c9f63
Sh@drun <5242001+sh0derun@users.noreply.github.com>
2023-01-15 21:12:35 +0100

    moving phong shading to light glsl header

--- shaders/fs_icosphere.glsl
+++ shaders/fs_icosphere.glsl
@@ -12,22 +12,8 @@ out vec4 FragColor;
 uniform Material model_material;
 uniform PointLight point_light;
 
-vec3 getPhongMaterial(PointLight pointLight){
-    vec3 ambientComponent =  vec3((0.212671f*model_material.ambient.r + 0.715160f*model_material.ambient.g + 0.072169f*model_material.ambient.b)/(0.212671f*model_material.diffuse.r + 0.715160f*model_material.diffuse.g + 0.072169f*model_material.diffuse.b));//model_material.ambient;
-    ambientComponent *= model_material.ambient;
-    vec3 lightDir = normalize(pointLight.position - outPosition);
-    float normalLightAngle = max(0, dot(outNormal, lightDir));
-    vec3 diffuseComponent = model_material.diffuse * pointLight.color * normalLightAngle;
-
-    vec3 from_light_source = -lightDir;
-    vec3 reflected_light = normalize(reflect(from_light_source, outNormal));
-    float specularFactor = pow(max(0, dot(normalize(-outPosition), reflected_light)), model_material.shininess);
-    vec3 specularComponent = model_material.specular * specularFactor * pointLight.color;
-
-    return vec3(ambientComponent + diffuseComponent + specularComponent);
-}
 
 void main() {
-    vec3 phongRes = getPhongMaterial(point_light);
+    vec3 phongRes = getPhongMaterial(model_material.ambient, model_material.diffuse, model_material.specular, model_material.shininess, point_light, outPosition, outNormal);
     FragColor = vec4(phongRes, 1);
 }
--- shaders/fscube.glsl
+++ shaders/fscube.glsl
@@ -3,6 +3,7 @@
 #include<material.glsl>
 #include<pointlight.glsl>
 
+
 out vec4 FragColor;
 
 in vec3 position;
@@ -12,22 +13,7 @@ uniform Material model_material;
 uniform PointLight point_light;
 
 
-vec3 getPhongMaterial(PointLight pointLight, float shininess){
-    vec3 ambientComponent =  vec3((0.212671f*model_material.ambient.r + 0.715160f*model_material.ambient.g + 0.072169f*model_material.ambient.b)/(0.212671f*model_material.diffuse.r + 0.715160f*model_material.diffuse.g + 0.072169f*model_material.diffuse.b));//model_material.ambient;
-    ambientComponent *= model_material.ambient;
-    vec3 lightDir = normalize(pointLight.position - position);
-    float normalLightAngle = max(0, dot(normal, lightDir));
-    vec3 diffuseComponent = model_material.diffuse * pointLight.color * normalLightAngle;
-
-    vec3 from_light_source = -lightDir;
-    vec3 reflected_light = normalize(reflect(from_light_source, normal));
-    float specularFactor = pow(max(0, dot(normalize(-position), reflected_light)), model_material.shininess);
-    vec3 specularComponent = model_material.specular * specularFactor * pointLight.color;
-
-    return vec3(ambientComponent + diffuseComponent + specularComponent);
-}
-
 void main() {
-    vec3 phongRes = getPhongMaterial(point_light, 64);
+    vec3 phongRes = getPhongMaterial(model_material.ambient, model_material.diffuse, model_material.specular, model_material.shininess, point_light, position, normal);
     FragColor = vec4(phongRes, 1);
 }
